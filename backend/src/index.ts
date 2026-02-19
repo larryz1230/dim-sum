fc390f613c06c737ccc97fc984be2a77158175d0
Aland Liu <52473359+alandliu@users.noreply.github.com>
2026-02-18 17:47:32 -0800

    Working ping test

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,7 +9,7 @@ const httpServer = http.createServer(app);
 
 const io = new Server(httpServer, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: "*", // TODO: change
         methods: ["GET", "POST"]
     }
 });
