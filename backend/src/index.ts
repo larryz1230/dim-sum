eac8c53d7a3ae12e90b48abd433756f095fe041a
Aland Liu <52473359+alandliu@users.noreply.github.com>
2026-02-18 17:28:17 -0800

    Testing pings

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,18 @@
 import express,  { Request, Response } from 'express';
+import http from 'http';
+import { Server } from "socket.io";
+import { SocketEvents } from "./utils/SocketEvents"; 
 
 const app = express();
+
+const httpServer = http.createServer(app);
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"]
+    }
+});
 
 app.use(express.json());
 
@@ -8,6 +20,17 @@ app.get('/', (req: Request, res: Response) => {
     res.json({ status: "Server is running." });
 })
 
-app.listen(4000, () => {
-    console.log("Server running on http://localhost:4000");
+io.on('connection', (socket) => {
+    console.log("User connected: ", socket.id);
+    socket.on(SocketEvents.Ping, () => {
+        socket.emit("pong");
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected: ", socket.id);
+    });
 });
+
+httpServer.listen(8080, () => {
+    console.log("Server running on port 8080: http://localhost:8080");
+})
