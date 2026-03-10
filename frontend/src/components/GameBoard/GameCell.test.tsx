5408c3fac5f0c07ab3100aad47a1d4d0d13bda9f
spmy <quorcx@gmail.com>
2026-03-10 11:37:22 -0700

    gamecell test & react testing library

---
+++ frontend/src/components/GameBoard/GameCell.test.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { describe, it, expect } from "vitest";
+import { GameCell } from "./GameCell.jsx";
+
+// GameCell
+describe("GameCell.empty", () => {
+    it("renders an empty cell when value is 0", () => {
+        const emptyCell = { value: 0 };
+        const { container } = render(<GameCell cell={emptyCell} isSelected={false}/>);
+        const cell = container.firstChild;
+
+        expect(cell).toHaveClass("game-cell");
+        expect(cell).toHaveClass("game-cell--empty");
+    })
+});
+
+describe("GameCell.nonempty", () => {
+    it("renders a non-empty cell when value is not 0", () => {
+        const emptyCell = { row: 1, col: 2, value: 5 };
+        const { container } = render(<GameCell cell={emptyCell} isSelected={false}/>);
+        const cell = container.firstChild;
+
+        expect(cell).not.toHaveClass("game-cell--empty");
+        expect(cell.firstChild).toHaveClass("game-cell__value");
+        expect(screen.getByText("5")).toBeInTheDocument();
+    })
+});
